ee8e17187b64fc2bf385003f90834c76fafbb5e6
Joaomacosdev <joamarcosdev@gmail.com>
2025-12-01 19:59:37 -0300

    primeiro commit

---
+++ src/main/java/br/com/altacommerce/AltacommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.altacommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltacommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltacommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/altacommerce/AltacommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.altacommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltacommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
